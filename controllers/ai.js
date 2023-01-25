e17bcb8cdbe912515e035a8d40a80c90d89a9502
Agu C. Christopher <aguchris740@gmail.com>
2023-01-25 21:35:20 +0100

    Update ai.js

--- controllers/ai.js
+++ controllers/ai.js
@@ -36,7 +36,7 @@ export const chat = async (req, res) => {
       prompt: prompt,
       model: "text-davinci-003",
     });
-    if (prompt.includes("your name")) {
+    if (prompt.includes("name") && prompt.includes("your")) {
      reply = "Hi, my name is Chris-GPT"
       }     
     res.status(200).json({
